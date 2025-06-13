a38e17f1bb2453c76b42fc77e26632b2f1a25ca5
Vinis-vp <viniciusviertel955@gmail.com>
2025-06-13 16:37:13 -0300

    feat: Criado o create e findAll de usuario.

--- backend/src/usuario/usuario.service.ts
+++ backend/src/usuario/usuario.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor (
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>
+  ) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto): Promise<Usuario> {
+    const user = await this.usuarioRepository.create(createUsuarioDto)
+    await this.usuarioRepository.save(user)
+    return user;
   }
 
-  findAll() {
-    return `This action returns all usuario`;
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find()
   }
 
   findOne(id: number) {
