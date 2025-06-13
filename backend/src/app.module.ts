ef45e132ce5dbb4a95aa5987752de95422ec4cf1
Vinis-vp <viniciusviertel955@gmail.com>
2025-06-13 16:36:25 -0300

    feat: Criado a conexão com o banco de dados.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
-import { UsuarioModule } from './usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/**/*.entity.js'],
+      synchronize: false,
+      timezone: 'Z',
+    }),
+    
+    UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
